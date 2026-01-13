400c089807a3a74dfccce2537a4fba8c33d0f149
omarsalahmohsen48 <omarsalahmohsen48@gmail.com>
2026-01-13 19:48:51 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -3,7 +3,24 @@ import numpy as np
 from scipy.signal import convolve2d
 
 def load_image(path):
-    pass # Replace the `pass` with your code
+    img = Image.open(path).convert('L')  
+    return np.array(img)
 
 def edge_detection(image):
-    pass # Replace the `pass` with your code
+    
+    Kx = np.array([[1, 0, -1],
+                   [2, 0, -2],
+                   [1, 0, -1]])
+    Ky = np.array([[1, 2, 1],
+                   [0, 0, 0],
+                   [-1, -2, -1]])
+    
+
+    gx = convolve2d(image, Kx, mode='same', boundary='symm')
+    gy = convolve2d(image, Ky, mode='same', boundary='symm')
+    
+
+    edges = np.sqrt(gx**2 + gy**2)
+
+    edges = (edges / edges.max()) * 255
+    return edges.astype(np.uint8)
